6c98dfd78d5f9d6c35e01bffecd6a78ebb77a98b
Yang Wang <yangwang@Yangs-MacBook-Air.local>
2024-12-26 22:48:02 +0100

    add buttons and functions

---
+++ index.js
@@ -0,0 +1,35 @@
+let scoreHomeEl = document.getElementById("score-home")
+let scoreGuestEl = document.getElementById("score-guest")
+
+let scoreHome = 0
+let scoreGuest = 0
+
+function addOneHome() {
+    scoreHome += 1
+    scoreHomeEl.textContent = scoreHome
+}
+
+function addTwoHome() {
+    scoreHome += 2
+    scoreHomeEl.textContent = scoreHome
+}
+
+function addThreeHome() {
+    scoreHome += 3
+    scoreHomeEl.textContent = scoreHome
+}
+
+function addOneGuest() {
+    scoreGuest += 1
+    scoreGuestEl.textContent = scoreGuest
+}
+
+function addTwoGuest() {
+    scoreGuest += 2
+    scoreGuestEl.textContent = scoreGuest
+}
+
+function addThreeGuest() {
+    scoreGuest += 3
+    scoreGuestEl.textContent = scoreGuest
+}
